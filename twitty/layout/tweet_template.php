93107b06d9481a6e8714bfcdb09e606a181bb51f
Thomas GARAYT <Thomas GARAYT>
2015-11-20 12:01:33 +0100

    Signed-off-by: Thomas GARAYT <Thomas GARAYT>

---
+++ twitty/layout/tweet_template.php
@@ -0,0 +1,26 @@
+<div>
+	<div class="tweet">
+		<table class="tweet-info">
+			<tr>
+				<td style="width:40px"><img width="40px" src="img/profil.png" /></td>
+				<td><span>NOM Prénom</span></td>
+				<td><span>11/11/11</span></td>
+				<td class="td-droite"><span>10 personnes aiment </span></td>
+
+			</tr>
+		</table>
+		
+		<div class="tweet-message">
+			Voici le message d'un tweet	
+		</div>
+		
+		<div class="tweet-image">		
+			<img src="img/profil.png" />
+		</div>
+		
+		<div class="tweet-button">
+			<input type="button" value="+1"></input>
+			<input type="button" value="Partager"></input>
+		</div>
+	</div>
+</div>
--- twitty/view/viewProfileSuccess.php
+++ twitty/view/viewProfileSuccess.php
@@ -1,36 +1,22 @@
 <h1>Informations sur NOM Prénom</h1>
 
 <div id="div-grand-profil">
-	<span>NOM</span>
-	<span>Prénom</span>
-	<span>Etudiant en L3 Informatique au CERI !</span>
+	<table>
+		<tr>
+			<td><img width="40px" src="img/profil.png" /></td>
+			<td><span>NOM Prénom</span></td>
+		</tr>
+		<tr>
+			<td colspan="2">Etudiant en L3 Informatique au CERI !</td>
+		</tr>
+	</table>
 </div>
 
 
 <h1>Tweets</h1>
 	<!-- Liste des tweets de l'utilisateur -->
-	<div>
-		<div class="tweet">
-			<table style="margin:0;">
-				<tr>
-					<td><img width="40px" src="img/profil.png" /></td>
-					<td>NOM Prénom</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td>le 11/11/11</td>
-				</tr>
-
-				<tr>
-					<td colspan="2">
-						<span>Blablablablablablablablablabla</span>
-					</td>
-				</tr>
-
-				<tr>
-					<td>+1</td>
-					<td>Share</td>
-				</tr>
-			</table>
-		</div>
-	</div>
+<?php
+	for($boucleTweet = 0 ; $boucleTweet < 5 ; $boucleTweet++) {
+		include($nameApp."/layout/tweet_template.php");
+	}
+?>
